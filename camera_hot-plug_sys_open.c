5f10256aed2462c3926f9e4cf16a3a2cbfd89b39
kevin <jucksunyi@163.com>
2018-06-27 08:37:09 +0800

    Update camera_hot-plug_sys_open.c

--- camera_hot-plug_sys_open.c
+++ camera_hot-plug_sys_open.c
@@ -1,3 +1,14 @@
+1:tasklet and workqueue
+2:	client->flags |= I2C_CLIENT_PEC;
+		/*
+		 * Setting the PEC flag here won't affect kernel drivers,
+		 * which will be using the i2c_client node registered with
+		 * the driver model core.  Likewise, when that client has
+		 * the PEC flag already set, the i2c-dev driver won't see
+		 * (or use) this setting.
+		 */
+3:A hot plug can trigger three interruptions that require a sleep function
+
 /*
  * MAXIM  max9286-max96705 GMSL driver
  *
